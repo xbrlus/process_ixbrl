ca13c690857bebe8b7fa61704764564b08346e0b
David Tauriello <david.tauriello@xbrl.us>
2022-09-23 17:36:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from ixbrl import XbrliDocument
 
 try:
 	ALLOWED_EXTENSIONS = set(['html','htm'])
-	baseDomain = 'http://ec2-43-205-125-1.ap-south-1.compute.amazonaws.com:5000/'
+	baseDomain = 'http://localhost:5000/'
 	uploadDirectory = 'upload/'
 	uploadDirectoryAbsolutePath = Path('upload').absolute()
  
